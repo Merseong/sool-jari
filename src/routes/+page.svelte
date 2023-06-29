a1eda4577c46a8087226a920b92d623000442753
Merseong <esc990720@korea.ac.kr>
2023-06-29 17:12:20 +0900

    Add html to image save function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,39 @@
 <script>
-	let a = 0;
-	$: a2 = a * 2;
+	import { toBlob } from 'html-to-image';
+	import { saveAs } from 'file-saver';
+
+	let searchValue = '';
+	let main;
+
+	function search() {
+		console.log(searchValue);
+	}
+
+	function download() {
+		toBlob(main).then((blob) => {
+			if (window.saveAs) {
+				window.saveAs(blob, 'my-image.png');
+			} else {
+				saveAs(blob, 'my-image.png');
+			}
+		});
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>{a}</p>
-<p>{a2}</p>
-<button on:click={() => a++}>+1</button>
+<div class="main" bind:this={main}>
+	<h1>SoolJari</h1>
+	<h3>이세상 모든 술들</h3>
+
+	<input type="text" bind:value={searchValue} placeholder="검색할 술을 입력하세요" />
+	<button on:click={search}>검색</button>
+	<p>{searchValue}</p>
+</div>
+
+<button on:click={download}>다운로드</button>
+
+<style>
+	.main {
+		background: white;
+		border: 1px solid black;
+	}
+</style>
