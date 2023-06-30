151580c7f359dd63f07069101679c0e87b0873f9
Merseong <esc990720@korea.ac.kr>
2023-06-30 15:21:13 +0900

    Add crud on test db

--- src/routes/test/+page.server.js
+++ src/routes/test/+page.server.js
@@ -1,6 +1,32 @@
 import { testDb } from '$lib/server/dbBase';
+import { fail } from '@sveltejs/kit';
 
 export const load = async () => {
 	const res = await testDb.findMany();
 	return { tests: res };
+};
+
+export const actions = {
+	create: async ({ cookies, request }) => {
+		const formData = await request.formData();
+		const title = formData.get('title');
+		const content = formData.get('content');
+
+		if (!title) {
+			return fail(400, {
+				error: 'Title must be need'
+			});
+		}
+
+		const result = await testDb.create({
+			data: {
+				title,
+				content
+			}
+		});
+
+		console.log(result);
+
+		return { success: true };
+	}
 };
