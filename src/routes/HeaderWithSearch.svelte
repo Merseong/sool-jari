724f5156a0f0902798f4aef512184d4fb5265c6c
Merseong <esc990720@korea.ac.kr>
2023-07-10 21:06:53 +0900

    Fix main designs

--- src/routes/HeaderWithSearch.svelte
+++ src/routes/HeaderWithSearch.svelte
@@ -6,9 +6,9 @@
 
 <nav class="fixed left-0 right-0 top-0 border-b border-black p-0">
   <div class="flex flex-row">
-    <a href="/">home</a>
+    <a class="flex items-center justify-center border border-black" href="/">home</a>
     <div class="mx-auto">
-      <input class="w-96" type="text" bind:value={searchKey} />
+      <input class="w-[20rem]" type="text" bind:value={searchKey} />
       <button
         on:click={() => {
           if (!searchKey.length) return;
--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -10,11 +10,11 @@
   }
 </script>
 
-<div class="container mx-auto content-center p-2">
-  <h1 class="text-center text-2xl">SoolJari</h1>
-  <h3 class="text-center text-lg">이세상 모든 술들</h3>
+<div class="container mx-auto mt-24 content-center p-2">
+  <h1 class="text-center text-4xl font-black">SoolJari</h1>
+  <h3 class="text-center text-lg">이세상 모든 술들(을 모으고싶은)</h3>
 
-  <div class="flex h-14 content-center text-center">
+  <div class="mx-auto flex h-14 max-w-3xl content-center text-center">
     <form class="w-[90%] p-0 pr-2" on:submit|preventDefault={search}>
       <input
         class="m-0 flex h-full w-full rounded-md border"
